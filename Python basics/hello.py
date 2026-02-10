ee1c3df303a61fffe250139211f0ab9f4a820dc1
kartik0920 <kartik0920@gmail.com>
2026-02-10 06:00:35 +0530

    Remove python basics to convert into submodule

--- Python basics/hello.py
+++
@@ -1,9 +0,0 @@
-def main():
-    name = "kartik"
-    print(f"Hello world {name}")
-    # I can use pip freeze > requirement.txt to get requimenet file
-    print("setup done")
-
-
-if __name__ == "__main__":
-    main()
